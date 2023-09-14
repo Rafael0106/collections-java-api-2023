de3fef6941de6d8e254e41f41efb2c6d14cae548
Rafael Sobral Coutinho <125751433+Rafael0106@users.noreply.github.com>
2023-09-14 15:28:35 -0300

    Add CatalogoLivros e Livro

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefa {
-    //atributo
+    // atributo
 
     private List<Tarefa> tarefaList;
 
@@ -12,29 +12,30 @@ public class ListaTarefa {
         this.tarefaList = new ArrayList<>();
     }
 
-    //adicionando tarefa na lista
-    public void adicionarTarefa(String descricao){
+    // adicionando tarefa na lista
+    public void adicionarTarefa(String descricao) {
         tarefaList.add(new Tarefa(descricao));
     }
 
-    //removendo a tarefa com tal descrição, ou seja, irá remover todas as tarefas caso bata com a descrição
-    public void removerTarefa(String descricao){
+    // removendo a tarefa com tal descrição, ou seja, irá remover todas as tarefas
+    // caso bata com a descrição
+    public void removerTarefa(String descricao) {
         List<Tarefa> tarefasParaRemover = new ArrayList<>();
-        for (Tarefa t : tarefaList){
-            if (t.getDescricao().equalsIgnoreCase(descricao)){
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
                 tarefasParaRemover.add(t);
             }
         }
         tarefaList.removeAll(tarefasParaRemover);
     }
 
-    public int obterNumeroTotalTarefas(){
-        //quantidade de elementos que tenho na lista
+    public int obterNumeroTotalTarefas() {
+        // quantidade de elementos que tenho na lista
         return tarefaList.size();
     }
 
-    public void obterDescricoesTarefas(){
-        //imprimindo uma lista de tarefas
+    public void obterDescricoesTarefas() {
+        // imprimindo uma lista de tarefas
         System.out.println(tarefaList);
     }
 
